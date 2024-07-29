47c316dd1679946d45e1d092d4c1b911fb25a56b
lupe0614 <lupejara0614@gmail.com>
2024-07-29 16:22:32 -0700

    Initial commit

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbVnlQghG21HPBfzJ9DkyXzBk8rmb311o",
+  authDomain: "gamehive-lj.firebaseapp.com",
+  projectId: "gamehive-lj",
+  storageBucket: "gamehive-lj.appspot.com",
+  messagingSenderId: "477397891351",
+  appId: "1:477397891351:web:06b24f0ab3b48ddf33df52"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

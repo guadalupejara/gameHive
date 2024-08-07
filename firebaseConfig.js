500c28e726583aaaeabf11d8c9d9a78c2723c64f
Guadalupe Jara <135558981+guadalupejara@users.noreply.github.com>
2024-08-06 18:05:13 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,13 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBbVnlQghG21HPBfzJ9DkyXzBk8rmb311o",
-  authDomain: "gamehive-lj.firebaseapp.com",
-  projectId: "gamehive-lj",
-  storageBucket: "gamehive-lj.appspot.com",
-  messagingSenderId: "477397891351",
-  appId: "1:477397891351:web:06b24f0ab3b48ddf33df52"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

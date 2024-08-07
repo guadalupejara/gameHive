9a836935b096d166b3df7575af8b391909551138
lupe0614 <lupejara0614@gmail.com>
2024-08-06 17:58:42 -0700

    chore- configured backend to accept cloud function, successfully tested client side function

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
+/* eslint-disable no-undef */
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBbVnlQghG21HPBfzJ9DkyXzBk8rmb311o",
-  authDomain: "gamehive-lj.firebaseapp.com",
-  projectId: "gamehive-lj",
-  storageBucket: "gamehive-lj.appspot.com",
-  messagingSenderId: "477397891351",
-  appId: "1:477397891351:web:06b24f0ab3b48ddf33df52"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const firestoreDb = getFirestore(app);
+
+export { app, firestoreDb };
+
